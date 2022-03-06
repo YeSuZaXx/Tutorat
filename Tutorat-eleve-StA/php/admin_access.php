d988d4779f2a5549a396c3e2ad797b178b8f3446
synthe <96080542+YeSuZaXx@users.noreply.github.com>
2022-03-06 18:18:56 +0100

    bdd autoincrement fix

--- Tutorat-eleve-StA/php/admin_access.php
+++ Tutorat-eleve-StA/php/admin_access.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="/style/table.css"/>
+    <link rel="stylesheet" type="text/css" href="../style/table.css"/>
     <style>
         body, h1, h2, h3, h4, h5, h6 {
             font-family: "Lato", sans-serif
